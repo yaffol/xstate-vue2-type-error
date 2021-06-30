c4dd3c86ffec6579211be02d3cb1b9f77a2d67c7
Patrick Vale <customintelligence@gmail.com>
2021-06-30 09:43:41 +0100

    style: adds Crossref theme colours

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#3eb1c8',
+        secondary: '#4f5858',
+        accent: '#3eb1c8',
+        error: '#ef3340',
+        info: '#2972fa',
+        success: '#00ab84',
+        warning: '#ffc72c'
+      }
+    }
+  }
 })
