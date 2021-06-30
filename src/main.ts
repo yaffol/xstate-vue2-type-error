609b53cdd1af1dc170ec4ee34a25a71dbbcf1d0c
Patrick Vale <customintelligence@gmail.com>
2021-06-30 13:00:40 +0100

    chore: enables composition api

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
+import { inspect } from '@xstate/inspect'
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(VueCompositionAPI)
+
+inspect({ iframe: false })
 
 Vue.config.productionTip = false
 
