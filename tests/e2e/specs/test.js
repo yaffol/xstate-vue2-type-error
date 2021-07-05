5109a89458cb154c599d32bc067db699260727d4
Patrick Vale <customintelligence@gmail.com>
2021-07-05 10:15:03 +0100

    test(cypress): Adds cypress run script, fixes example test to target Vuetify default template text

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,10 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('My First Test', {
+  baseUrl: 'http://localhost:8082'
+}, () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.contains('h1', 'Welcome to Vuetify')
   })
 })
